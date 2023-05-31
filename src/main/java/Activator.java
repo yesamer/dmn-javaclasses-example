8b1954ba2a8cb40d5ca4f596571b6a8d3cc4ed59
Yeser Amer <yeser.amer@ibm.com>
2023-05-31 12:53:32 +0200

    Update to Kogito 1.39.0 and removing @KieActivator

--- src/main/java/Activator.java
+++ src/main/java/Activator.java
@@ -13,9 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import org.kie.api.project.KieActivator;
 
-@KieActivator
 public class Activator {
     
 }
